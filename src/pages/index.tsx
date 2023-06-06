9c0d55a29493590a1913b0e855ad2793d80866b9
Tomczan <t.czuba95@gmail.com>
2023-06-06 13:49:55 +0200

    Add dayjs and next image tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,12 @@ import Link from "next/link";
 
 import { RouterOutputs, api } from "~/utils/api";
 
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import Image from "next/image";
+
+dayjs.extend(relativeTime);
+
 const CreatePostWizard = () => {
   const { user } = useUser();
 
@@ -12,10 +18,12 @@ const CreatePostWizard = () => {
 
   return (
     <div className="flex w-full gap-4">
-      <img
+      <Image
         src={user.profileImageUrl}
-        alt="Profile Image"
+        alt={`@${user.username}'s profile picture`}
         className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
       />
       <input
         type="text"
@@ -32,11 +40,20 @@ const PostView = (props: PostWithUser) => {
   const { post, author } = props;
   return (
     <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
-      <img src={author.profilePicture} className="h-14 w-14 rounded-full" />
+      <Image
+        src={author.profilePicture}
+        className="h-14 w-14 rounded-full"
+        alt={`Author ${author.username}'s profile picture`}
+        width={56}
+        height={56}
+      />
       <div className="flex flex-col">
         <div className="flex items-center justify-center gap-1 text-slate-500">
           <span className="font-bold text-slate-200">{author.username}</span>
-          <span className="">{`@${author.username}`}</span>
+          <span className="font-thin">
+            {`@${author.username}`}{" "}
+            <span>{` · ${dayjs(post.createdAt).fromNow()}`}</span>
+          </span>
         </div>
         <span>{post.content}</span>
       </div>
