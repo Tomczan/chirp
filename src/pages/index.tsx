abe6a4e13e40a6cb2cd00ee6120b26c392d9529a
Tomczan <t.czuba95@gmail.com>
2023-06-06 14:23:56 +0200

    template literal expression fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ const CreatePostWizard = () => {
     <div className="flex w-full gap-4">
       <Image
         src={user.profileImageUrl}
-        alt={`@${user.username}'s profile picture`}
+        alt={`Your profile picture`}
         className="h-14 w-14 rounded-full"
         width={56}
         height={56}
