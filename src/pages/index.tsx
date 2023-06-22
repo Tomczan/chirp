dc96092fb1393be30106dd31d346bc0a16cfca52
Tomczan <t.czuba95@gmail.com>
2023-06-22 13:51:49 +0200

    PostView link to post detail covers whole area

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -87,36 +87,37 @@ const PostView = (props: PostWithUser) => {
   const { post, author } = props;
 
   return (
-    <div
-      key={post.id}
-      className="flex gap-3 border-b border-slate-400 p-4 hover:cursor-pointer hover:bg-gray-900"
-    >
-      <Image
-        src={author.profilePicture}
-        className="h-14 w-14 rounded-full"
-        alt={`Author ${author.username}'s profile picture`}
-        width={56}
-        height={56}
-      />
-      <div className="flex flex-col ">
-        <div className="flex gap-1 text-slate-500">
-          <Link href={`/@${author.username}`}>
-            <span className="font-bold text-slate-200 hover:underline">
-              {author.firstName && author.lastName
-                ? `${author.firstName} ${author.lastName}`
-                : author.username}
-            </span>
-          </Link>
-          <Link href={`/post/${post.id}`}>
+    <Link href={`/post/${post.id}`}>
+      <div
+        key={post.id}
+        className="flex gap-3 border-b border-slate-400 p-4 hover:cursor-pointer hover:bg-gray-900"
+      >
+        <Image
+          src={author.profilePicture}
+          className="h-14 w-14 rounded-full"
+          alt={`Author ${author.username}'s profile picture`}
+          width={56}
+          height={56}
+        />
+        <div className="flex flex-col">
+          <div className="flex gap-1 text-slate-500">
+            <Link href={`/@${author.username}`}>
+              <span className="font-bold text-slate-200 hover:underline">
+                {author.firstName && author.lastName
+                  ? `${author.firstName} ${author.lastName}`
+                  : author.username}
+              </span>
+            </Link>
+
             <span className="font-thin">
               {`@${author.username}`}{" "}
               <span>{` · ${dayjs(post.createdAt).fromNow()}`}</span>
             </span>
-          </Link>
+          </div>
+          <span className="text-2xl">{post.content}</span>
         </div>
-        <span className="text-2xl">{post.content}</span>
       </div>
-    </div>
+    </Link>
   );
 };
 
